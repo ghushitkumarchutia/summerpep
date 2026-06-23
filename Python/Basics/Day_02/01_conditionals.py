0e09630b49c135e7eef73ddaf1ba35900b452f18
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-06-23 12:48:28 +0530

    feat(day2-python): add prime number checking logic

--- Python/Basics/Day_02/01_conditionals.py
+++ Python/Basics/Day_02/01_conditionals.py
@@ -31,3 +31,21 @@ elif employee_salary < 70000:
     print("Employee is in the upper salary bracket")
 else:
     print("Employee is in the high salary bracket")
+
+# Check prime number or not
+number = int(input("Enter your number: "))
+
+if number <=1:
+    print("Number is not prime.")
+else:
+    is_prime = True
+
+    for i in range(2, number):
+        if(number % i == 0):
+            is_prime = False
+            break
+
+    if is_prime:
+        print("Number is prime.")
+    else:
+        print("Number is not prime.")
