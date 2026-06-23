a60df60a39bcff9d7f487963bfd99d2b1f274ae5
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-06-23 12:23:14 +0530

    feat(day2-python): add conditional control flow statements

---
+++ Python/Basics/Day_02/01_conditionals.py
@@ -0,0 +1,33 @@
+# Conditional statement : 
+
+# Voting eligibility using conditional statement
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+# Positive and Negative number using conditional statement
+number = int(input("Enter your number: "))
+
+if number == 0:
+    print("Number is zero")
+elif number > 0:
+    print("It is a positive number")
+else:
+    print("It is a negative number")
+
+# Categorize employees based on salaries
+employee_salary = int(input("Enter your salary: "))
+
+if employee_salary <=0:
+    print("Salary cannot be zero or negative.")
+elif employee_salary < 30000:
+    print("Employee is in the lower salary bracket")
+elif employee_salary < 50000:
+    print("Employee is in the middle salary bracket")
+elif employee_salary < 70000:
+    print("Employee is in the upper salary bracket")
+else:
+    print("Employee is in the high salary bracket")
