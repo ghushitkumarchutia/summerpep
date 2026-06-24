4daa16f38819bf384e3ae726bb778f5b5e51bd29
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-06-24 14:08:35 +0530

    feat(day3-python): add demonstrations of break, continue, and pass statements

---
+++ Python/Basics/Day_03/03_break_continue.py
@@ -0,0 +1,17 @@
+# Example of break:
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i)
+
+# Example of continue:
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i)
+
+# Example of pass:
+for i in range(1,11):
+    if i == 5:
+        pass
+    print(i)    
