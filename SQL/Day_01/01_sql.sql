917200d34ebabc4640dff312368860f1ba0d8f64
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-06-26 14:45:08 +0530

    feat(day2-sql): add SELECT queries with inequality and logical AND conditions

--- SQL/Day_01/01_sql.sql
+++ SQL/Day_01/01_sql.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE lpu_students; -- Creates the database named lpu_students
 
-use lpu_students; -- Selects the database named lpu_students
+USE lpu_students; -- Selects the database named lpu_students
---
+++ SQL/Day_02/01_sql.sql
@@ -0,0 +1,10 @@
+SELECT * FROM lpu_students WHERE city != 'Houston';
+
+SELECT * FROM lpu_students WHERE branch != 'Information Technology';
+
+SELECT * FROM lpu_students WHERE cgpa < 8.0;
+
+SELECT * FROM lpu_students WHERE city != 'Houston' and city != 'San Jose';
+
+SELECT * FROM lpu_students WHERE branch != 'Computer Science' and branch != 'Business Administration';
+
